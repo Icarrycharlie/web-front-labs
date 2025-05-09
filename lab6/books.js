fd7796509866178a0e925232a607a98901f7a936
Максим Гришти <maxgrixti@yandex.ru>
2025-05-10 03:17:41 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,98 @@
+const books = [
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 380,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Виктор Пелевин',
+        name: 'Generation П',
+        publicationYear: 1999,
+        price: 560,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Дмитрий Глуховский',
+        name: 'Метро 2033',
+        publicationYear: 2005,
+        price: 620,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Гузель Яхина',
+        name: 'Зулейха открывает глаза',
+        publicationYear: 2015,
+        price: 590,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 680,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Борис Пастернак',
+        name: 'Доктор Живаго',
+        publicationYear: 1957,
+        price: 420,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Борис Акунин',
+        name: 'Азазель',
+        publicationYear: 1998,
+        price: 530,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Захар Прилепин',
+        name: 'Обитель',
+        publicationYear: 2014,
+        price: 670,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Евгений Водолазкин',
+        name: 'Лавр',
+        publicationYear: 2012,
+        price: 580,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Владимир Сорокин',
+        name: 'День опричника',
+        publicationYear: 2006,
+        price: 610,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Людмила Улицкая',
+        name: 'Казус Кукоцкого',
+        publicationYear: 2001,
+        price: 490,
+        bestseller: true,
+        available: true
+    }
+];
